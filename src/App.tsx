f207356a220203942cd153a58a7ef3bfcb093b0a
Matheus-Venancio <matheusvecordeiro@gmail.com>
2024-09-17 13:21:23 -0300

    Finalização do projeto sem Next e sem Firebase

--- src/App.tsx
+++ src/App.tsx
@@ -5,39 +5,49 @@ import styles from './App.module.css'
 import TaskForm from './components/Form/TaskForm';
 import TaskList from './components/List/TaskList';
 
-import {ITask} from './interfaces/ITask'
+import { ITask } from './interfaces/ITask'
 import Modal from './components/Modal/Modal';
 
 function App() {
 
   const [taskList, setTaskList] = useState<ITask[]>([])
-  const [taskToUpdate, setTaskToUpdate] =useState<ITask | null>(null);
+  const [taskToUpdate, setTaskToUpdate] = useState<ITask | null>(null);
 
-  const deleteTask =(id: number) => {
-    setTaskList(taskList.filter((task) => {return task.id !== id;}));
+  const deleteTask = (id: number) => {
+    setTaskList(taskList.filter((task) => { return task.id !== id; }));
   }
   const hideOrShowModal = (display: boolean) => {
     const modal = document.querySelector("#modal")
-    if(display){
+    if (display) {
       modal!.classList.remove("hide")
-    }else{
+    } else {
       modal!.classList.add("hide")
     }
   }
 
-  const editTask = (task:ITask):void =>{
+  const editTask = (task: ITask): void => {
     hideOrShowModal(true)
     setTaskToUpdate(task)
   }
 
+  const updateTask = (id: number, title: string, difficulty: number): void => {
+    const updatedTask: ITask = { id, title, difficulty }
+    //verifica cada dados da lista. quando acha o id, ele troca os dados
+    const updateItems = taskList.map((task) => {
+      return task.id === updatedTask.id ? updatedTask : task
+    })
+
+    setTaskList(updateItems)
+    hideOrShowModal(false)
+  }
   return <div>
-    <Modal children={<TaskForm btnText='Editar Tarefa' task={taskToUpdate} taskList={taskList}/>} />
+    <Modal children={<TaskForm btnText='Editar Tarefa' task={taskToUpdate} taskList={taskList} handleUpdate={updateTask}/>} />
     <Header />
     <main className={styles.main}>
       <h2>O que você vai fazer?</h2>
-      <TaskForm  btnText='Criar Tarefa' taskList={taskList} setTaskList={setTaskList}/>
+      <TaskForm btnText='Criar Tarefa' taskList={taskList} setTaskList={setTaskList} />
       <h2>Suas Tarefas:</h2>
-      <TaskList taskList={taskList} handleDelete={deleteTask} handleEdit={editTask}/>
+      <TaskList taskList={taskList} handleDelete={deleteTask} handleEdit={editTask} />
     </main>
     <Footer />
   </div>
--- src/components/Form/TaskForm.tsx
+++ src/components/Form/TaskForm.tsx
@@ -1,22 +1,23 @@
-import React,{useState,ChangeEvent,FormEvent,useEffect} from 'react'
+import React, { useState, ChangeEvent, FormEvent, useEffect } from 'react'
 import styles from './TaskForm.module.css'
-import {ITask} from '../../interfaces/ITask'
+import { ITask } from '../../interfaces/ITask'
 
 type Props = {
-    btnText: string
-    taskList: ITask[]
-    setTaskList?: React.Dispatch<React.SetStateAction<ITask[]>>
-    task?: ITask | null;
+  btnText: string
+  taskList: ITask[]
+  setTaskList?: React.Dispatch<React.SetStateAction<ITask[]>>
+  task?: ITask | null;
+  handleUpdate?(id: number, title: string, difficulty: number): void
 }
 
-const TaskForm = ({btnText,taskList,setTaskList,task}: Props) =>{
+const TaskForm = ({ btnText, taskList, setTaskList, task, handleUpdate }: Props) => {
 
-  const [id,setId] =useState<number>(0)
-  const [title,setTitle] =useState<string>("")
-  const [difficulty,setdifficulty] =useState<number>(0)
+  const [id, setId] = useState<number>(0)
+  const [title, setTitle] = useState<string>("")
+  const [difficulty, setdifficulty] = useState<number>(0)
 
-  useEffect(()=>{
-    if(task){
+  useEffect(() => {
+    if (task) {
       setId(task.id)
       setTitle(task.title)
       setdifficulty(task.difficulty)
@@ -24,36 +25,40 @@ const TaskForm = ({btnText,taskList,setTaskList,task}: Props) =>{
   }, [task])
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    if(e.target.name === "title"){
-        setTitle(e.target.value)
-    }else{
-        setdifficulty(Number(e.target.value))
+    if (e.target.name === "title") {
+      setTitle(e.target.value)
+    } else {
+      setdifficulty(Number(e.target.value))
     }
   }
   const addTaskHandler = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const id =Math.floor(Math.random() * 1000)
-    const newTask: ITask={id,title,difficulty}
+    if (handleUpdate) {
+      handleUpdate(id,title,difficulty)
+    } else {
+      const id = Math.floor(Math.random() * 1000)
+      const newTask: ITask = { id, title, difficulty }
 
-    //Esta adicionando todas as task e a new task. A ! fala que é certeza que vai vir
-    setTaskList!([...taskList,newTask])
+      //Esta adicionando todas as task e a new task. A ! fala que é certeza que vai vir
+      setTaskList!([...taskList, newTask])
 
-    setTitle("")
-    setdifficulty(0)
+      setTitle("")
+      setdifficulty(0)
+    }
   }
 
   return (
     <form onSubmit={addTaskHandler} className={styles.form}>
-        <div className={styles.input_container}>
-            <label htmlFor='title'>Título</label>
-            <input value={title} type="text" name="title" placeholder='Titulo da tarefa' onChange={handleChange}/>
-        </div>
-        <div className={styles.input_container}>
-            <label htmlFor='difficulty'>Dificuldade</label>
-            <input value={difficulty} type="text" name="difficulty" placeholder='Dificuldade da tarefa'  onChange={handleChange}/>
-        </div>
-        <input type='submit' value={btnText} />
+      <div className={styles.input_container}>
+        <label htmlFor='title'>Título</label>
+        <input value={title} type="text" name="title" placeholder='Titulo da tarefa' onChange={handleChange} />
+      </div>
+      <div className={styles.input_container}>
+        <label htmlFor='difficulty'>Dificuldade</label>
+        <input value={difficulty} type="text" name="difficulty" placeholder='Dificuldade da tarefa' onChange={handleChange} />
+      </div>
+      <input type='submit' value={btnText} />
     </form>
   )
 }

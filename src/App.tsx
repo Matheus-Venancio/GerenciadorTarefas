098463645ca8a321de6c06a850aa7ea91385406d
Matheus-Venancio <matheusvecordeiro@gmail.com>
2024-09-16 22:32:02 -0300

    Criação do formulario de tarefas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 import styles from './App.module.css'
+import TaskForm from './components/Form/TaskForm';
+import TaskList from './components/List/TaskList';
 
 function App() {
   return <div>
     <Header />
-    <main className={styles.main}></main>
+    <main className={styles.main}>
+      <TaskForm btnText='Criar Tarefa'/>
+      <TaskList />
+    </main>
     <Footer />
   </div>
 }
---
+++ src/components/Form/TaskForm.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from './TaskForm.module.css'
+type Props = {btnText: string}
+
+function TaskForm({btnText}: Props) {
+  return (
+    <form className={styles.form}>
+        <div className={styles.input_container}>
+            <label htmlFor='title'>Título</label>
+            <input type="text" name="title" placeholder='Titulo da tarefa' />
+        </div>
+        <div className={styles.input_container}>
+            <label htmlFor='difficulty'>Dificuldade</label>
+            <input type="text" name="difficulty" placeholder='Dificuldade da tarefa' />
+        </div>
+        <input type='submit' value={btnText} />
+    </form>
+  )
+}
+
+export default TaskForm

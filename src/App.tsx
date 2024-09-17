9f4891f5160ea43151e496aa2410ac6299d154e8
Matheus-Venancio <matheusvecordeiro@gmail.com>
2024-09-17 13:12:11 -0300

    Adicionando os valores ao modal na hora de editar

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,38 @@ import TaskForm from './components/Form/TaskForm';
 import TaskList from './components/List/TaskList';
 
 import {ITask} from './interfaces/ITask'
+import Modal from './components/Modal/Modal';
 
 function App() {
 
   const [taskList, setTaskList] = useState<ITask[]>([])
+  const [taskToUpdate, setTaskToUpdate] =useState<ITask | null>(null);
 
   const deleteTask =(id: number) => {
     setTaskList(taskList.filter((task) => {return task.id !== id;}));
   }
+  const hideOrShowModal = (display: boolean) => {
+    const modal = document.querySelector("#modal")
+    if(display){
+      modal!.classList.remove("hide")
+    }else{
+      modal!.classList.add("hide")
+    }
+  }
+
+  const editTask = (task:ITask):void =>{
+    hideOrShowModal(true)
+    setTaskToUpdate(task)
+  }
 
   return <div>
+    <Modal children={<TaskForm btnText='Editar Tarefa' task={taskToUpdate} taskList={taskList}/>} />
     <Header />
     <main className={styles.main}>
       <h2>O que você vai fazer?</h2>
-      <TaskForm btnText='Criar Tarefa' taskList={taskList} setTaskList={setTaskList}/>
+      <TaskForm  btnText='Criar Tarefa' taskList={taskList} setTaskList={setTaskList}/>
       <h2>Suas Tarefas:</h2>
-      <TaskList taskList={taskList} handleDelete={deleteTask}/>
+      <TaskList taskList={taskList} handleDelete={deleteTask} handleEdit={editTask}/>
     </main>
     <Footer />
   </div>
--- src/components/Form/TaskForm.tsx
+++ src/components/Form/TaskForm.tsx
@@ -6,13 +6,22 @@ type Props = {
     btnText: string
     taskList: ITask[]
     setTaskList?: React.Dispatch<React.SetStateAction<ITask[]>>
+    task?: ITask | null;
 }
 
-const TaskForm = ({btnText,taskList,setTaskList}: Props) =>{
+const TaskForm = ({btnText,taskList,setTaskList,task}: Props) =>{
 
   const [id,setId] =useState<number>(0)
   const [title,setTitle] =useState<string>("")
   const [difficulty,setdifficulty] =useState<number>(0)
+
+  useEffect(()=>{
+    if(task){
+      setId(task.id)
+      setTitle(task.title)
+      setdifficulty(task.difficulty)
+    }
+  }, [task])
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     if(e.target.name === "title"){
